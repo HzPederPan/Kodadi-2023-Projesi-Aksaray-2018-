804b887895ef3bdf3322cb67b7f7dfb0f342f62f
HzPederPan <panasoniickalempilarda@gmail.com>
2018-08-05 11:52:50 +0300

    mobil uygulama tasarımı başladı

---
+++ Mobil/to do list/js/my-app.js
@@ -0,0 +1,54 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('#baslik').html("Burası Hakkında Sayfası !")
+    $$('#icerik').html("Merhaba Ben Arda Doğanyiğit ! Bu uygulamanın Yaratıcısı olan Türküm. Bu Uygulamayı T.C. Gençlik ve Spor Bakanığının Kod Adı 2023 Aksaray Yaz Kodlama Eğitim Kamp'nda geliştirdim. Uygulamam bir tür To Do List ya da Türkçesile Yapılacaklar Listesi.")
+});
+
+myApp.onPageInit('form', function (page) {
+    // run createContentPage func after link was clicked
+    $$('#baslik').html("Burası Hakkında Sayfası !")
+    $$('#icerik').html("Merhaba Ben Arda Doğanyiğit ! Bu uygulamanın Yaratıcısı olan Türküm. Bu Uygulamayı T.C. Gençlik ve Spor Bakanığının Kod Adı 2023 Aksaray Yaz Kodlama Eğitim Kamp'nda geliştirdim. Uygulamam bir tür To Do List ya da Türkçesile Yapılacaklar Listesi.")
+});
+
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
